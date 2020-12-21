eca8aad106ec324b7ee66e8473a36d5b9c86b178
Prajwal Shah <prajwalshah1721@gmail.com>
2020-12-21 17:47:42 +0530

    Other changes

--- dbms/src/App.js
+++ dbms/src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import {EmployeePage} from './pages/Employee/employee';
 import {WarehousePage} from './pages/warehouse/warehouse';
+import {NavbarLeft} from './components/Navbar/navbarLeft';
+import {EmployeeInsert} from './pages/Employee/employee.insert';
+import {EmployeeSearch} from './pages/Employee/employee.search';
+import {NavbarRight} from './components/Navbar/navbarRight';
 import { Switch,Route} from 'react-router-dom';
-import {Navbar} from './components/Navbar/navbar';
+
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavbarLeft/>
       <Switch>
+        {/* employee routing */}
+        <Route  exact component={EmployeeInsert} path={'/employee/insert'}/> 
+        <Route  exact component={EmployeeSearch} path={'/employee/search'}/>  
         <Route exact component={EmployeePage} path='/employee'/>
         <Route exact component={WarehousePage} path='/warehouse'/>
         <Route path='/'/>
       </Switch>
-      <div>Hello</div>
+      <NavbarRight/>
     </div>
   );
 }
