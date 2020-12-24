892842a135f330185827c27abe1f733880f7eebe
Prajwal Shah <prajwalshah1721@gmail.com>
2020-12-24 23:03:26 +0530

    Bug resolved

--- dbms/src/components/Display/display.jsx
+++ dbms/src/components/Display/display.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 export const Display=({Data})=>{
     return (
         <div>
+            {console.log(Data)}
         {
-            Object.keys(Data[0]).map((t)=>
-                    <span>{t}</span>
-                )
-        }
+            Data.length>0?
+                Object.keys(Data[0]).map((t)=>
+                <span>{t}</span>):<></>
+        } 
         {
+            Data.length>0?
             Data.map((data)=>
                 Object.values(data).map((t)=>
                     <span>{t}</span>
                 )
-            )
+            ):<></>
         }
         </div>
     )
